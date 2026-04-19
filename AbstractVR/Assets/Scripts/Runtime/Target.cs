db2c32d78ed2a7ace5db8bfe45e230db03f39893
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2026-04-19 11:17:08 +0100

    added assembly definitions

---
+++ AbstractVR/Assets/Scripts/Runtime/Target.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    const string HITBOX_NAME = "TargetHitbox";
+    public static int Count { get; private set; }
+    
+    [SerializeField] float score = 10f;
+    [SerializeField] float maxHealth = 1f;
+    [SerializeField] GameObject hitVFXPrefab;
+
+    MeshRenderer[] meshRenderers;
+    Collider hitbox;
+
+    float destroyTime;      // 0  or taken from hitVFXPrefab's particle system duration
+    ParticleSystem ps;
+
+
+
+    void Awake()
+    {
+        Debug.Log("Target Awake");
+        // subscribe to particle system's OnComplete event to destroy the target after the hitVFXPrefab's particle system finishes
+        FindRefereces();
+    }
+
+    private void FindRefereces()
+    {
+        // Cache references to components for performance optimization
+        meshRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        // Find the hitbox to use
+        // First find by name in children, then fallback to GetComponentInChildren if not found
+        Transform hitboxTransform = transform.Find(HITBOX_NAME);
+        if (hitboxTransform != null) hitbox = hitboxTransform.GetComponent<Collider>();
+        else                         hitbox = GetComponentInChildren<Collider>();
+        
+        if (hitVFXPrefab != null)
+        {
+            ps = hitVFXPrefab.GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                destroyTime = ps.main.duration;
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        Count++;
+        Debug.Log("Target OnEnable, Count: " + Count);
+    }
+    
+    void OnDisable()
+    {
+        Count--;
+        Debug.Log("Target OnDisable, Count: " + Count);
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("Target OnDestroy, Count: " + Count);
+    }
+}
